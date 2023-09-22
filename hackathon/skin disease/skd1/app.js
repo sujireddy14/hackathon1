f0ed824c6eb48de99badcdda692130bcf47510d5
sujireddy14 <129601809+sujireddy14@users.noreply.github.com>
2023-09-22 12:14:58 +0530

    Add files via upload

---
+++ hackathon/skin disease/skd1/app.js
@@ -0,0 +1,11 @@
+var abc=document.getElementById('image')
+if(abc){
+    image=='s1.jpg';
+    console.log(image)
+    console.log(ACNE)
+}
+else if(abc){
+    image=='s2.jpg';
+    console.log(image)
+    console.log('ROSACEA');
+}
